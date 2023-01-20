edb42d4299f9bb75848bfe1188eee89235e09461
Ivan Milosavljevic <verthygo@gmail.com>
2023-01-20 23:55:28 +0100

    references

--- src/Servers.cs
+++ src/Servers.cs
@@ -5,7 +5,6 @@ public class Servers
     public static readonly string PostgresConnectionString =
         "Host=localhost;Port=5433;Database=postgres;Username=postgres;password=postgres";
 
-    public static readonly string SqlServerConnectionString = "Server=localhost,1434;User Id=sa;Password=P@55w0rd;Timeout=5;MultipleActiveResultSets=True;Initial Catalog=master;TrustServerCertificate=true";
-
-
+    public static readonly string SqlServerConnectionString =
+        "Server=localhost,1434;User Id=sa;Password=P@55w0rd;Timeout=5;MultipleActiveResultSets=True;Initial Catalog=master;TrustServerCertificate=true";
 }
