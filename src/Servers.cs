5ed5df3633d1607ac898d467a572b6bb3535cb38
Ivan Milosavljevic <verthygo@gmail.com>
2023-01-20 23:55:13 +0100

    references

--- src/Servers.cs
+++ src/Servers.cs
@@ -1,13 +1,11 @@
-﻿using Microsoft.Data.SqlClient;
-
-namespace IntegrationTests;
+﻿namespace IntegrationTests;
 
 public class Servers
 {
     public static readonly string PostgresConnectionString =
         "Host=localhost;Port=5433;Database=postgres;Username=postgres;password=postgres";
 
-    public static readonly string SqlServerConnectionString = new SqlConnectionStringBuilder("Server=localhost,1434;User Id=sa;Password=P@55w0rd;Timeout=5;MultipleActiveResultSets=True;Initial Catalog=master;TrustServerCertificate=true").ConnectionString;
+    public static readonly string SqlServerConnectionString = "Server=localhost,1434;User Id=sa;Password=P@55w0rd;Timeout=5;MultipleActiveResultSets=True;Initial Catalog=master;TrustServerCertificate=true";
 
 
 }
