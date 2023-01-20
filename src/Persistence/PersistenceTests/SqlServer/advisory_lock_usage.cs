7b4a12ce7b6be9e13ca37a576ae959c66b4d3368
Ivan Milosavljevic <verthygo@gmail.com>
2023-01-20 23:54:48 +0100

    references

--- src/Persistence/PersistenceTests/SqlServer/advisory_lock_usage.cs
+++ src/Persistence/PersistenceTests/SqlServer/advisory_lock_usage.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Data.SqlClient;
 using IntegrationTests;
-using Microsoft.Data.SqlClient;
 using Shouldly;
 using Wolverine.SqlServer;
 using Xunit;
--- src/Servers.cs
+++ src/Servers.cs
@@ -1,10 +1,13 @@
-﻿namespace IntegrationTests;
+﻿using Microsoft.Data.SqlClient;
+
+namespace IntegrationTests;
 
 public class Servers
 {
     public static readonly string PostgresConnectionString =
         "Host=localhost;Port=5433;Database=postgres;Username=postgres;password=postgres";
 
-    public static readonly string SqlServerConnectionString =
-        "Server=localhost,1434;User Id=sa;Password=P@55w0rd;Timeout=5;MultipleActiveResultSets=True;Initial Catalog=master;Encrypt=True;TrustServerCertificate=True";
+    public static readonly string SqlServerConnectionString = new SqlConnectionStringBuilder("Server=localhost,1434;User Id=sa;Password=P@55w0rd;Timeout=5;MultipleActiveResultSets=True;Initial Catalog=master;TrustServerCertificate=true").ConnectionString;
+
+
 }
